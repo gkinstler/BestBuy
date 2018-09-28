00c4430e40b366388e62d4455801950a6b65241f
Glenn Kinstler <glenn@glennkinstler.com>
2018-09-28 14:04:52 -0500

    First Commit

---
+++ BestBuy/Cameras.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestBuy
+{
+    class Cameras
+    {
+        public int Resolution { get; set; }
+        public string lens { get; set; }
+        public bool HasCase { get; set; }
+        public bool HasAutoFocus { get; set; }
+    }
+}
